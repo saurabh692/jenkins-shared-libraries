e79cd2ec5e9f7716cf2d845256df18f3ff7a1225
Saurabh Kumbhar <76954871+saurabh692@users.noreply.github.com>
2026-03-15 23:51:58 +0530

    Update and rename docker-push.groovy to dockerpush.groovy

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -3,6 +3,6 @@ def call(String credentialsId, String imageName, String tag) {
     withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DockerHubUsername', passwordVariable: 'DockerHubPassword')]) {
         sh "docker login -u ${DockerHubUsername} -p ${DockerHubPassword}"
         sh "docker image tag ${imageName}:latest ${DockerHubUsername}/${imageName}:${tag}"
-        sh "docker push ${DockerHubUsername}/${imageName}:${tag}""
+        sh "docker push ${DockerHubUsername}/${imageName}:${tag}"
     }
 }
